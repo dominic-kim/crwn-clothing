644601e4de4284dfc4e8c242de28756943c0ea22
Dom Kim <domkim@gmail.com>
2021-06-25 13:06:36 -0400

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,7 +9,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
-      <span className='subtitle'>{match.url}</span>
     </div>
   </div>
 );
