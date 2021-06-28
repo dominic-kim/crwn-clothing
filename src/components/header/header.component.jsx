36168c23e7217482091a7908ab67c399032a9a3b
Dom Kim <domkim@gmail.com>
2021-06-28 16:39:17 -0400

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,12 +10,12 @@ import './header.styles.scss';
 
 const Header = ({currentUser}) => (
   <div className='header'>
-    <Link className='logo-container' to="/">
+    <Link className='logo-container' to='/'>
       <Logo className ='logo' />
     </Link>
     <div className='options'>
-      <Link className='option'>SHOP</Link>
-      <Link className='option'>CONTACT</Link>
+      <Link className='option' to='/shop'>SHOP</Link>
+      <Link className='option' to='/'>CONTACT</Link>
       {
         currentUser ? <div className='option' onClick={() => auth.signOut()} >SIGN OUT</div> : 
         <Link className='option' to='/signin'>SIGN IN</Link>
