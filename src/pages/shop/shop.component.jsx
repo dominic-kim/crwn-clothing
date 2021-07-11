86895d7dbf82d300eba53116ff0fc762689fb57d
Dom <dominic.kim@trackernetworks.com>
2021-07-11 09:48:05 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
+
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
@@ -14,11 +15,14 @@ class ShopPage extends React.Component {
 
   render() {
     const {collections} = this.state;
-    return (<div className='shop-page'>
-      {collections.map(({id, ...otherCollectionProps}) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>);
+    
+    return (
+      <div className='shop-page'>
+        {collections.map(({id, ...otherCollectionProps}) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
